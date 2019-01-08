eee3488414b9117c41def6ce31b012111886dc9f
Ramiro Andrés <ramiroandres@Ramiros-MacBook-Pro.local>
2019-01-08 15:24:25 -0500

    first commit

---
+++ src/config/dev.js
@@ -0,0 +1,7 @@
+export const FirebaseConfig =  {
+    apiKey: "API_KEY",
+    authDomain: "DOMAIN.firebaseapp.com",
+    databaseURL: "https://DOMAIN.firebaseio.com",
+    projectId: "PROJECT_ID",
+    messagingSenderId: "MESSAGING_SENDER_ID"
+};
