39d6da2ed88fa04e754b9d40ad999880f1b74e70
Ramiro Andrés <ramiroandres@Ramiros-MacBook-Pro.local>
2019-01-08 15:32:12 -0500

    update config

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig =  {
+    apiKey: "AIzaSyBd_CoczlcStQXin1vdbmqSYFUckZiDeOU",
+    authDomain: "todos-bedac.firebaseapp.com",
+    databaseURL: "https://todos-bedac.firebaseio.com",
+    projectId: "todos-bedac",
+    storageBucket: "todos-bedac.appspot.com",
+    messagingSenderId: "980760347339"
+};
